1f7b75fa4777c6f01e1bab21fa48efd4e9a744cc
sruthins007 <sruthins007@gmail.com>
2018-02-24 16:01:54 +0530

    Add files via upload

---
+++ sec_mark.aspx
@@ -0,0 +1,103 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin.Master" AutoEventWireup="true" CodeBehind="sec_mark.aspx.cs" Inherits="QusMaster.sec_mark" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+
+     <div class="row mt">
+                    <div class="col-lg-12">
+          			<div class="form-panel">
+                  	  <h4 class="mb"><i class="fa fa-angle-right"></i>Section Mark Registration</h4>
+                      <%--<form class="form-inline" role="form">--%>
+                          <div>
+                      <table class="table" align="left">
+
+                         
+
+                         
+                      <tr>
+                      <td valign="middle">
+                       Scheme
+                       </td>
+                       <td valign="middle">
+                      <div class="form-group">
+                         <asp:DropDownList ID="ddlscheme" AppendDataBoundItems="true" runat="server" AutoPostBack="true" CssClass="form-control" OnSelectedIndexChanged="ddlscheme_SelectedIndexChanged" >
+                              <asp:ListItem Value="0">Select</asp:ListItem>                      
+                     </asp:DropDownList>   
+                       </div>
+                      </td>
+                      </tr>
+
+                    
+                                 
+
+
+                            <%-- <asp:TextBox ID="txtdtrecmm" runat="server" CssClass="datepicker" style="width:325px !important;" />  --%>
+                                      
+                          
+
+<tr>
+
+              <asp:gridview ID="Gridview1" runat="server" ShowFooter="True" AutoGenerateColumns="False" OnSelectedIndexChanged="Gridview1_SelectedIndexChanged" CellPadding="4" ForeColor="#333333" GridLines="None" >
+
+                  <AlternatingRowStyle BackColor="White" />
+
+        <Columns>
+
+        <asp:BoundField DataField="RowNumber" HeaderText="Row Number" />
+
+        <asp:TemplateField HeaderText="Section">
+
+            <ItemTemplate>
+
+                <asp:TextBox ID="TextBox1" runat="server" CssClass="form-control"></asp:TextBox>
+
+            </ItemTemplate>
+
+        </asp:TemplateField>
+
+        <asp:TemplateField HeaderText="Max. Mark">
+
+            <ItemTemplate>
+
+                <asp:TextBox ID="TextBox2" runat="server" CssClass="form-control"></asp:TextBox>
+
+            </ItemTemplate>
+
+        </asp:TemplateField>
+
+        <asp:TemplateField HeaderText="Total Mark">
+
+            <ItemTemplate>
+
+                 <asp:TextBox ID="TextBox3" runat="server" CssClass="form-control"></asp:TextBox>
+
+            </ItemTemplate>
+
+            <FooterStyle HorizontalAlign="Right" />
+
+            
+
+        </asp:TemplateField>
+
+        </Columns>
+
+                  <FooterStyle BackColor="#990000" Font-Bold="True" ForeColor="White" />
+                  <HeaderStyle BackColor="#990000" Font-Bold="True" ForeColor="White" />
+                  <PagerStyle BackColor="#FFCC66" ForeColor="#333333" HorizontalAlign="Center" />
+                  <RowStyle BackColor="#FFFBD6" ForeColor="#333333" />
+                  <SelectedRowStyle BackColor="#FFCC66" Font-Bold="True" ForeColor="Navy" />
+                  <SortedAscendingCellStyle BackColor="#FDF5AC" />
+                  <SortedAscendingHeaderStyle BackColor="#4D0000" />
+                  <SortedDescendingCellStyle BackColor="#FCF6C0" />
+                  <SortedDescendingHeaderStyle BackColor="#820000" />
+
+</asp:gridview>
+    </tr>
+                          </table>
+            <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" />                   
+                    </div>
+                          </div>
+                        </div>
+        </div>
+
+</asp:Content>
---
+++ sec_mark.aspx.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace QusMaster
+{
+    public partial class sec_mark : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            if (!IsPostBack)
+            {
+                SqlConnection conn = new SqlConnection("Data Source=.\\SQLEXPRESS;Initial Catalog=qus;User ID=sa;Password=admin123");
+                conn.Open();
+                string comm = "Select scheme_name,scheme_id from tbl_scheme ";
+                SqlDataAdapter adptt = new SqlDataAdapter(comm, conn);
+                DataTable dtt = new DataTable();
+                adptt.Fill(dtt);
+                ddlscheme.DataSource = dtt;
+                ddlscheme.DataTextField = "scheme_name";
+                ddlscheme.DataValueField = "scheme_id";
+                ddlscheme.DataBind();
+                conn.Close();
+            }
+
+            //if (!Page.IsPostBack)
+            //{
+
+            //    SetInitialRow();
+
+            //}
+
+
+        }
+
+        private void SetInitialRow()
+        {
+
+            DataTable dt = new DataTable();
+
+            DataRow dr = null;
+
+            dt.Columns.Add(new DataColumn("RowNumber", typeof(string)));
+
+            dt.Columns.Add(new DataColumn("Column1", typeof(string)));
+
+            dt.Columns.Add(new DataColumn("Column2", typeof(string)));
+
+            dt.Columns.Add(new DataColumn("Column3", typeof(string)));
+
+            dr = dt.NewRow();
+
+            dr["RowNumber"] = 1;
+
+            dr["Column1"] = string.Empty;
+
+            dr["Column2"] = string.Empty;
+
+            dr["Column3"] = string.Empty;
+
+            dt.Rows.Add(dr);
+
+            //dr = dt.NewRow();
+
+
+
+            //Store the DataTable in ViewState
+
+            ViewState["CurrentTable"] = dt;
+
+
+
+            Gridview1.DataSource = dt;
+
+            Gridview1.DataBind();
+
+
+        }
+
+
+        private void AddNewRowToGrid()
+        {
+
+            int rowIndex = 0;
+
+
+
+            if (ViewState["CurrentTable"] != null)
+            {
+
+                DataTable dtCurrentTable = (DataTable)ViewState["CurrentTable"];
+
+                DataRow drCurrentRow = null;
+
+                if (dtCurrentTable.Rows.Count > 0)
+                {
+
+                    for (int i = 1; i <= dtCurrentTable.Rows.Count; i++)
+                    {
+
+                        //extract the TextBox values
+
+                        TextBox box1 = (TextBox)Gridview1.Rows[rowIndex].Cells[1].FindControl("TextBox1");
+
+                        TextBox box2 = (TextBox)Gridview1.Rows[rowIndex].Cells[2].FindControl("TextBox2");
+
+                        TextBox box3 = (TextBox)Gridview1.Rows[rowIndex].Cells[3].FindControl("TextBox3");
+
+
+
+                        drCurrentRow = dtCurrentTable.NewRow();
+
+                        drCurrentRow["RowNumber"] = i + 1;
+
+
+
+                        dtCurrentTable.Rows[i - 1]["Column1"] = box1.Text;
+
+                        dtCurrentTable.Rows[i - 1]["Column2"] = box2.Text;
+
+                        dtCurrentTable.Rows[i - 1]["Column3"] = box3.Text;
+
+
+
+                        rowIndex++;
+
+                    }
+
+                    dtCurrentTable.Rows.Add(drCurrentRow);
+
+                    ViewState["CurrentTable"] = dtCurrentTable;
+
+
+
+                    Gridview1.DataSource = dtCurrentTable;
+
+                    Gridview1.DataBind();
+
+                }
+
+            }
+
+            else
+            {
+
+                Response.Write("ViewState is null");
+
+            }
+
+
+
+            //Set Previous Data on Postbacks
+
+            SetPreviousData();
+
+        }
+
+        private void SetPreviousData()
+        {
+
+            int rowIndex = 0;
+
+            if (ViewState["CurrentTable"] != null)
+            {
+
+                DataTable dt = (DataTable)ViewState["CurrentTable"];
+
+                if (dt.Rows.Count > 0)
+                {
+
+                    for (int i = 0; i < dt.Rows.Count; i++)
+                    {
+
+                        TextBox box1 = (TextBox)Gridview1.Rows[rowIndex].Cells[1].FindControl("TextBox1");
+
+                        TextBox box2 = (TextBox)Gridview1.Rows[rowIndex].Cells[2].FindControl("TextBox2");
+
+                        TextBox box3 = (TextBox)Gridview1.Rows[rowIndex].Cells[3].FindControl("TextBox3");
+
+
+
+                        box1.Text = dt.Rows[i]["Column1"].ToString();
+
+                        box2.Text = dt.Rows[i]["Column2"].ToString();
+
+                        box3.Text = dt.Rows[i]["Column3"].ToString();
+
+
+
+                        rowIndex++;
+
+                    }
+
+                }
+
+            }
+
+        }
+
+        protected void Gridview1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=.\\SQLEXPRESS;Initial Catalog=qus;User ID=sa;Password=admin123");
+            con.Open();
+
+            foreach (GridViewRow gr in Gridview1.Rows)
+            {
+                if (gr.RowType == DataControlRowType.DataRow)
+                {
+                    TextBox textBox1 = gr.FindControl("TextBox1") as TextBox;
+                    TextBox textBox2 = gr.FindControl("TextBox2") as TextBox;
+                    TextBox textBox3 = gr.FindControl("TextBox3") as TextBox;
+
+                    string sql = "insert into tbl_sec_mark(scheme_id,section,max_mark,total_mark) values('" + ddlscheme.SelectedItem.Value + "','" + textBox1.Text.Trim() + "','" + textBox2.Text.Trim() + "','" + textBox3.Text.Trim() + "')";
+                    SqlCommand cmd = new SqlCommand(sql, con);
+                    cmd.ExecuteNonQuery();
+
+
+                }
+            }
+
+            
+
+            ddlscheme.ClearSelection();
+            
+           
+
+
+
+            Response.Redirect("sec_mark.aspx");
+
+        }
+
+        protected void ddlscheme_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SetInitialRow();
+
+
+            SqlConnection con = new SqlConnection("Data Source=.\\SQLEXPRESS;Initial Catalog=qus;User ID=sa;Password=admin123");
+            con.Open();
+            string com = "SELECT section from tbl_scheme where scheme_id = '" + ddlscheme.SelectedItem.Value + "' ";
+            SqlDataAdapter adpt = new SqlDataAdapter(com, con);
+            DataTable dt = new DataTable();
+            adpt.Fill(dt);
+
+            DataView dv = dt.DefaultView;
+            int abc = 0;
+            foreach (DataRowView drv in dv)
+            {
+                abc = Convert.ToInt32(drv.Row["section"]);
+            }
+
+            for (int i = 1; i <abc; i++)
+            {
+                AddNewRowToGrid();
+            }
+        }
+        
+    }
+}
