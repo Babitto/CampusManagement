1a07a94fac99c0d9ac4c4b85e2e73a1b1af30489
blossombabu7 <36694572+blossombabu7@users.noreply.github.com>
2018-03-06 14:51:55 +0530

    Add files via upload

---
+++ admin_mail.aspx
@@ -0,0 +1,64 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin.Master" AutoEventWireup="true" CodeBehind="admin_mail.aspx.cs" Inherits="QuesGen.admin_mail" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="row mt">
+                    <div class="col-lg-12">
+          			<div class="form-panel">
+                  	  <h4 class="mb"><i class="fa fa-angle-right"></i>Mailing</h4>
+                      <%--<form class="form-inline" role="form">--%>
+                          <div>
+                      <table class="table" align="left">
+                           <tr>
+                      <td valign="middle">
+                       Department
+                       </td>
+                       <td valign="middle">
+                      <div class="form-group">
+                          <asp:DropDownList ID="ddldept" runat="server" CssClass="form-control"></asp:DropDownList> 
+                      </div>
+                      </td>
+                      </tr>
+                          <tr>
+                      <td valign="middle">
+                       Subject
+                       </td>
+                       <td valign="middle">
+                      <div class="form-group">
+                          <asp:TextBox ID="txtsub" runat="server" CssClass="form-control"></asp:TextBox>
+                       </div>
+                      </td>
+                      </tr>
+                      <tr>
+                      <td valign="middle">
+                       Message
+                       </td>
+                       <td valign="middle">
+                      <div class="form-group">
+                       <asp:TextBox ID="txtmessage" runat="server" CssClass="form-control" TextMode="MultiLine" />    
+                       </div>
+                      </td>
+                      </tr>
+
+                           
+                     
+                      </table>                
+
+                               
+   
+
+
+
+                            <%-- <asp:TextBox ID="txtdtrecmm" runat="server" CssClass="datepicker" style="width:325px !important;" />  --%>
+                                      
+                           <asp:Button runat="server" ID="btnSave" CssClass="btn btn-theme" 
+                            Width="100px" Text="Send" OnClick="btnSave_Click"  />
+                              
+                    </div>
+                          </div>
+                        </div>
+        </div>
+
+
+</asp:Content>
---
+++ admin_mail.aspx.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net.Mail;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace QuesGen
+{
+    public partial class admin_mail : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["qusmaster"].ConnectionString);
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                con.Open();
+                string com = "Select dept_id,dept_name from tbl_dept";
+                SqlDataAdapter adpt = new SqlDataAdapter(com, con);
+                DataTable dt = new DataTable();
+                adpt.Fill(dt);
+                ddldept.DataSource = dt;
+                ddldept.DataTextField = "dept_name";
+                ddldept.DataValueField = "dept_id";
+                ddldept.DataBind();
+
+                con.Close();
+            }
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            //string com = "Select email from tbl_staff";
+            //SqlDataAdapter adpt = new SqlDataAdapter(com, con);
+            //DataTable dt = new DataTable();
+            //adpt.Fill(dt);
+            String sql = "Select email,type from tbl_staff where type='STAFF' and dept_id='" + ddldept.SelectedValue + "'";
+            SqlDataReader rdr = null;
+            SqlCommand cmd = new SqlCommand(sql, con);
+            rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    string email = (string)rdr["email"];
+
+                    try
+                    {
+
+                        MailMessage mail = new MailMessage();
+                        SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+
+                        mail.From = new MailAddress("cmsystem.saintgits@gmail.com");
+                        mail.To.Add("cmsystem.saintgits@gmail.com");
+                        mail.To.Add(email);
+                        mail.Subject = txtsub.Text.Trim();
+                        mail.Body = txtmessage.Text.Trim();
+                        mail.IsBodyHtml = true;
+                        SmtpServer.Port = 587;
+                        SmtpServer.Credentials = new System.Net.NetworkCredential("cmsystem.saintgits@gmail.com", "cmsystem007");
+                        SmtpServer.EnableSsl = true;
+                        SmtpServer.Send(mail);
+                    }
+
+                    catch (Exception exp)
+                    {
+                        throw exp;
+                    }
+                }
+      //          String email = row["email"].ToString;
+
+      
+          
+
+        }
+    }
+}
