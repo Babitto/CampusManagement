c378ecdb822b871dcea355e4f4125319a800a8f5
sruthins007 <sruthins007@gmail.com>
2018-03-15 15:52:54 +0530

    Add files via upload

---
+++ search_staff.aspx
@@ -0,0 +1,59 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin.Master" AutoEventWireup="true" CodeBehind="search_staff.aspx.cs" Inherits="QuesGen.search_staff" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="row mt">
+                    <div class="col-lg-12">
+          			<div class="form-panel">
+                  	  <h4 class="mb"><i class="fa fa-angle-right"></i>Search Subject</h4>
+                      <%--<form class="form-inline" role="form">--%>
+                          <div>
+                      <table class="table" align="left">
+
+                         <tr>
+                      
+                       <td valign="middle" colspan="4">
+                      <div class="form-group">
+                          <asp:TextBox ID="txtSearch" runat="server" CssClass="form-control" placeholder="Search"></asp:TextBox>
+                       </div>
+                      </td>
+                             </tr>
+                          <tr>
+                        <td valign="middle">
+                      <div class="form-group">
+                          <asp:Button ID="Search1" runat="server" Text="Search" CssClass="form-control" OnClick="Search1_Click"   />
+                       </div>
+                      </td>
+                      </tr>
+                            
+                          <tr>
+                       
+                       <td valign="middle" colspan="3">
+                      <div class="form-group">
+                          &nbsp;&nbsp;
+                          <asp:GridView ID="gvsub" runat="server" AutoGenerateEditButton="True" Width="633px" OnRowEditing="gvsub_RowEditing" OnRowUpdating="gvsub_RowUpdating" OnRowCancelingEdit="gvsub_RowCancelingEdit"   DataKeyNames="staff_id" ></asp:GridView>
+                      </div>
+                          
+                      </td>
+                      </tr>
+
+                      
+ 
+                     
+                      </table>                
+
+                            &nbsp;&nbsp;   
+   
+
+
+
+                            <%-- <asp:TextBox ID="txtdtrecmm" runat="server" CssClass="datepicker" style="width:325px !important;" />  --%>
+                                      
+                           
+                    </div>
+                          </div>
+                        </div>
+        </div>
+
+</asp:Content>
---
+++ search_staff.aspx.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+using System.Text.RegularExpressions;
+
+namespace QuesGen
+{
+    public partial class search_staff : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!this.IsPostBack)
+            {
+                this.BindGrid();
+            }
+        }
+
+        private void BindGrid()
+        {
+            string constr = ConfigurationManager.ConnectionStrings["qusmaster"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.CommandText = "SELECT staff_id, staff_name, username, password, email, usertype_id FROM tbl_staff WHERE usertype_id=1 and staff_name LIKE '%" + txtSearch.Text.Trim() + "%'";
+                    cmd.Connection = con;
+                    cmd.Parameters.AddWithValue("@staff_name", txtSearch.Text.Trim());
+                    DataTable dt = new DataTable();
+                    using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+                    {
+                        sda.Fill(dt);
+                        gvsub.DataSource = dt;
+                        gvsub.DataBind();
+                    }
+                }
+            }
+        }
+
+
+        protected void Search1_Click(object sender, EventArgs e)
+        {
+            this.BindGrid();
+        }
+
+        protected void OnPageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            gvsub.PageIndex = e.NewPageIndex;
+            this.BindGrid();
+        }
+
+        protected void gvsub_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            gvsub.EditIndex = e.NewEditIndex;
+            this.BindGrid();
+        }
+
+        protected void gvsub_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            GridViewRow row = gvsub.Rows[e.RowIndex];
+            string test = gvsub.DataKeys[e.RowIndex].Values[0].ToString();
+            Int64 staff_id = Convert.ToInt64(gvsub.DataKeys[e.RowIndex].Values[0]);
+            string name = (row.Cells[2].Controls[0] as TextBox).Text;
+            string username = (row.Cells[3].Controls[0] as TextBox).Text;
+            string password = (row.Cells[4].Controls[0] as TextBox).Text;
+            string email = (row.Cells[5].Controls[0] as TextBox).Text;
+
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["qusmaster"].ConnectionString);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("UPDATE tbl_staff SET staff_name=@name, username=@username,password=@password,email=@email WHERE staff_id=@staff_id");
+            {
+
+                cmd.Parameters.AddWithValue("@staff_name", name);
+                cmd.Parameters.AddWithValue("@username", username);
+                cmd.Parameters.AddWithValue("@password", password);
+                cmd.Parameters.AddWithValue("@email", email);
+                cmd.Connection = con;
+                // con.Open();
+                cmd.ExecuteNonQuery();
+                // con.Close();
+            }
+            con.Close();
+            gvsub.EditIndex = -1;
+            this.BindGrid();
+        }
+
+        protected void gvsub_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            gvsub.EditIndex = -1;
+            this.BindGrid();
+        }
+    }
+}
